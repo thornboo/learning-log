e6cfbf563bd43ca25d1c959cb80809b54e096e9d
zhaozhuang <zhaozhuang1210@gmail.com>
2021-01-20 20:45:23 +0800

    Complete the first part

---
+++ learning_logs/urls.py
@@ -0,0 +1,14 @@
+"""定义learning_logs的URL模式"""
+from django.conf.urls import url
+from . import views
+
+app_name = 'learning_logs'
+
+urlpatterns = [
+    # 主页
+    url(r'^$', views.index, name='index'),
+    # 显示所有的主题
+    url(r'^topics/$', views.topics, name='topics'),
+    # 特定主题的详细页面
+    url(r'^topics/(?P<topic_id>\d+)/$', views.topic, name='topic'),
+]
